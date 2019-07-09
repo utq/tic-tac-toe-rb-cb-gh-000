b78a93d026a57ab56174e484df937a34d7d8a672
Celal Cankaya <celalcankaya@outlook.com>
2019-07-09 22:08:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,16 +16,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8],  # Middle row
-  [0,3,6], # Top row
-  [1,4,7],  # Middle row
-  [2,5,8],  # Middle row
-  [0,4,8], # Top row
-  [2,4,6],  # Middle row
-]
+WIN_COMBINATIONS = [[0,1,2], # Top row[3,4,5],  # Middle row[6,7,8],  # Middle row[0,3,6], # Top row[1,4,7],  # Middle row[2,5,8],  # Middle row[0,4,8], # Top row[2,4,6],  # Middle row]
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
